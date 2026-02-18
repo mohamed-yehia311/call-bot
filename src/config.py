8a902b9cd26f397a8dc82ab00f98db74d5e37a33
mohamed-yehia311 <mmoa311311@gmail.com>
2026-02-18 03:04:43 +0200

    feat: add configuration classes for Gemini and Superlinked settings

--- src/config.py
+++ src/config.py
@@ -9,10 +9,23 @@ class GeminiSettings(BaseModel):
     api_key: str = Field(default="", description="Gemini API Key")
     model: str = Field(default="gemini-2.5-flash", description="Gemini Model to use")
 
+# --- Superlinked configuration ---
+class SuperlinkedSettings(BaseModel):
+    embedding_model: str = Field(default="entence-transformers/all-MiniLM-L6-v2", description="Embedding model used by superlinked") 
+    sqm_min_value : int = Field(default=20, description="Minimum value for appartment size in square meter")
+    sqm_max_value : int = Field(default=2000, description="Maximum value for appartment size in square meter")
+    price_min_value: int = Field(default=100000, description="Minimum value for appartment price in EGP")
+    price_max_value: int = Field(default=10000000, description="Maximum value for appartment price in EGP")
+
+
 
 # --- Settings Configuration ---
 class Settings(BaseSettings):
     gemini: GeminiSettings = Field(default_factory=GeminiSettings)
+
+    superlinked: SuperlinkedSettings = Field(default_factory=SuperlinkedSettings)
+
+    
 
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
         env_file=[".env"],
